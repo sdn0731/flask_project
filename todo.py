e02d7474eaf0305542fd2a632e7f4ca3a9b3ddef
Sarah "Dasha" Nolan <snolan0731@gmail.com>
2023-02-20 20:42:59 +0000

    Added html page

--- todo.py
+++ todo.py
@@ -1,11 +1,28 @@
 #app.py
 
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
+
+
 
 @app.route("/")
 def hello():
-    return "Hello World"
+    return render_template('home.html')
+
+@app.route('/addTodo', method=['POST'])
+def addTodo():
+    #access body information
+
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
+
+
+
+
+
+
+
+#GET, PUT, POST, DELETE
+#GET and DELETE
+#CRUD CREATE, RETRIEVE, UPDATE, DELETE
